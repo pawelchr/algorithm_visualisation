1a8619ad55c8006af5c4ba0eb1fe9bbad6fdbb2d
melo1901 <meluszko@gmail.com>
2024-09-12 00:00:38 +0200

    last month various conceptions

--- leptos/algorithm-visualisation/src/sorting_chart.rs
+++ leptos/algorithm-visualisation/src/sorting_chart.rs
@@ -1,71 +1,297 @@
-use crate::sorting::BarColor;
+
+use crate::sorting::{BarColor, MyData, SortRes};
 use leptos::*;
-use leptos_charts::*;
-use std::f64;
+// use leptos_charts::{BarChart, Color, BarChartOptions, Palette};
+use leptos_chartistry::*;
+use leptos_dom::logging::console_log;
+// #[component]
+// pub fn SortingChart(
+//     steps: ReadSignal<Vec<Vec<f64>>>,
+//     palettes: ReadSignal<Vec<Vec<BarColor>>>,
+// ) -> impl IntoView {
+//     let (disable, set_disable) = create_signal(false);
+//     let (current_step, set_current_step) = create_signal(0);
+//     let grey_color = Color::RGB(108, 108, 108);
+//     let green_color = Color::RGB(0, 255, 0);
+//     let orange_color = Color::RGB(227, 150, 62);
+
+//     // Memoize the palettes so it doesn't recompute unnecessarily
+//     let palettes_memo = create_memo(move |_| palettes());
+
+//     // Define the palette for the current step
+//     let palette = create_memo(move |_| {
+//         let current_index = current_step();
+//         let step_palette = &palettes_memo()[current_index];
+//         step_palette
+//             .iter()
+//             .map(|color| match color {
+//                 BarColor::Grey => grey_color.clone(),
+//                 BarColor::Green => green_color.clone(),
+//                 _ => orange_color.clone(),
+//             })
+//             .collect::<Vec<Color>>()
+//     });
+
+//     let options = BarChartOptions { max_ticks: 4 };
+
+//     // Define the step values for the current step
+//     let step = create_memo(move |_| {
+//         let current_index = current_step();
+//         steps()[current_index].clone()
+//     });
+
+//     view! {
+//         <div class="flex items-center justify-center">
+//             <div class="items-center flex flex-auto justify-center basis-full">
+//                 <BarChart
+//                     values=Signal::derive(move || step()).into()
+//                     pallete=Signal::derive(move || Palette(palette())).into()
+//                     options=options
+//                     attr:style="margin-top:5px "
+//                     attr:preserveAspectRatio="none"
+//                     attr:width="500"
+//                     attr:height="500"
+//                 />
+//             </div>
+//             <div class="fixed bottom-0 items-center">
+//                 <button on:click=move |_| {
+//                     set_current_step.update(|n| *n -= 1);
+//                 }>"Previous step"</button>
+//                 <button
+//                     prop:disabled=move || { if disable() { true } else { false } }
+//                     on:click=move |_| {
+//                         if current_step() < steps().len() - 1 {
+//                             set_current_step.update(|n| *n += 1);
+//                         } else {
+//                             set_disable(true)
+//                         }
+//                     }
+//                 >
+//                     "Next step"
+//                 </button>
+//                 // <p>"step: "{move || step().to_string()}</p>
+//                 <p>"current step: "{move || current_step().to_string()}</p>
+//                 <p>"steps.len: "{move || steps().len().to_string()}</p>
+//             </div>
+//         </div>
+//     }
+// }
+
+// #[component]
+// pub fn SortingChartistry(
+//     debug: Signal<bool>,
+//     data: Signal<SortRes>,
+// ) -> impl IntoView {
+//     let grey_color = Colour::from_rgb(108, 108, 108);
+//     let green_color = Colour::from_rgb(0, 255, 0);
+//     let orange_color = Colour::from_rgb(227, 150, 62);
+
+//     let is_data_non_empty = Signal::derive(move || data().steps.data.len() != 0);
+//     // let data = Signal::derive(data);
+//     // // if is_data_non_empty.get() {
+//     // let step = data().steps.return_index(0).clone();
+//     // let first_colour = match data().steps.data[0][0].colour {
+//     //     BarColor::Grey => grey_color.clone(),
+//     //     BarColor::Green => green_color.clone(),
+//     //     _ => orange_color.clone(),
+//     // };
+
+//     // let scheme = ColourScheme::new(first_colour, vec![Colour::from_rgb(1, 1, 1)]);
+//     // let data_steps = create_rw_signal(data().steps.data[0].clone());
+//     // let step_clone = step.clone();
+//     // let series = Series::new(move |data: &MyData| step_clone[0].x.clone())
+//     //     .bar(move |data: &MyData| step[0].y1.clone()).with_colours(scheme);
+// //     view! {
+// //         <Chart
+// //             aspect_ratio=AspectRatio::from_outer_height(300.0, 1.2)
+// //             debug=debug
+// //             series=series
+// //             data=data_steps
+
+// //             left=TickLabels::aligned_floats()
+// //             inner=[
+// //                 AxisMarker::left_edge().into_inner(),
+// //                 AxisMarker::bottom_edge().into_inner(),
+// //                 YGridLine::default().into_inner(),
+// //             ]
+// //         />
+// //         // <div class="fixed bottom-0 items-center">
+// //         //     <button on:click=move |_| {
+// //         //         set_current_step.update(|n| *n -= 1);
+// //         //     }>"Previous step"</button>
+// //         //     <button
+// //         //         prop:disabled=move || { if disable() { true } else { false } }
+// //         //         on:click=move |_| {
+// //         //             if current_step() < steps().len() - 1 {
+// //         //                 set_current_step.update(|n| *n += 1);
+// //         //             } else {
+// //         //                 set_disable(true)
+// //         //             }
+// //         //         }
+// //         //     >
+// //         //         "Next step"
+// //         //     </button>
+// //         //     // <p>"step: "{move || step().to_string()}</p>
+// //         //     // <p>"current step: "{move || current_step().to_string()}</p>
+// //         //     // <p>"steps.len: "{move || steps().len().to_string()}</p>
+// //         // </div>
+// //     }
+// // }
+// view! {
+//     <div>
+//         {move || {
+//             if is_data_non_empty.get() {
+//                 let step = data().steps.return_index(0).clone();
+//                 let first_colour = match data().steps.data[0][0].colour {
+//                     BarColor::Grey => grey_color.clone(),
+//                     BarColor::Green => green_color.clone(),
+//                     _ => orange_color.clone(),
+//                 };
+//                 let scheme = ColourScheme::new(first_colour, vec![Colour::from_rgb(1, 1, 1)]);
+//                 let data_steps = create_rw_signal(data().steps.data[0].clone());
+//                 let step_clone = step.clone();
+//                 let series = Series::new(move |data: &MyData| step_clone[0].x.clone())
+//                     .bar(move |data: &MyData| step[0].y1.clone())
+//                     .with_colours(scheme);
+//                 view! {
+//                     <Chart
+//                         aspect_ratio=AspectRatio::from_outer_height(500.0, 1.2)
+//                         debug=debug
+//                         series=series
+//                         data=data_steps
+
+//                         left=TickLabels::aligned_floats()
+//                         inner=[
+//                             AxisMarker::left_edge().into_inner(),
+//                             AxisMarker::bottom_edge().into_inner(),
+//                             YGridLine::default().into_inner(),
+//                         ]
+//                     />
+//                 }
+//             } else {
+//                 view! {
+//                     <p>{move || format!("is not emtpy: {:?}", is_data_non_empty.get())}</p>
+//                     <p>"Enter numbers"</p>
+//                     <p>{move || format!("data {:?}", data.get())}</p>
+//                 }
+//                     .into_view()
+//             }
+//         }}
+//     </div>
+// }
+// }
 
 #[component]
-pub fn SortingChart(
-    steps: ReadSignal<Vec<Vec<f64>>>,
-    palettes: ReadSignal<Vec<Vec<BarColor>>>,
+pub fn SortingChartistry(
+    debug: Signal<bool>,
+    data: Signal<SortRes>,
 ) -> impl IntoView {
-    let (disable, set_disable) = create_signal(false);
-    let (current_step, set_current_step) = create_signal(0);
-    let grey_color = Color::RGB(108, 108, 108);
-    let green_color = Color::RGB(0, 255, 0);
-    let orange_color = Color::RGB(227, 150, 62);
+    let current_step = create_rw_signal(0);
 
-    // Memoize the palettes so it doesn't recompute unnecessarily
-    let palettes_memo = create_memo(move |_| palettes());
+    // Reset current_step when data changes
+    create_effect(move |_| {
+        data.track();
+        current_step.set(0);
+    });
 
-    // Define the palette for the current step
-    let palette = create_memo(move |_| {
-        let current_index = current_step();
-        let step_palette = &palettes_memo()[current_index];
-        step_palette
-            .iter()
-            .map(|color| match color {
-                BarColor::Grey => grey_color.clone(),
-                BarColor::Green => green_color.clone(),
-                _ => orange_color.clone(),
+    let current_data = create_memo(move |_| {
+        data.with(|d| {
+            d.steps.data.get(current_step.get())
+                .cloned()
+                .unwrap_or_default()
+        })
+    });
+
+    // let bars = create_rw_signal(move |_| {
+    //     current_data.with(|data| {
+    //         data.iter()
+    //             .enumerate()
+    //             .map(|(index, step)| {
+    //                 let y1 = step.y1;
+    //                 let color = match step.colour {
+    //                     BarColor::Orange => Colour::from_rgb(255, 127, 80),
+    //                     BarColor::Green => Colour::from_rgb(50, 205, 50),
+    //                     BarColor::Grey => Colour::from_rgb(192, 192, 192),
+    //                 };
+    //                 Bar::new(move |_: &MyData| y1).with_colour(color)
+    //             })
+    //             .collect::<Vec<_>>().iter()
+    //     })
+    // });
+
+    let bars_data = Signal::derive(move || {current_data.with(|data| {
+        data.iter()
+            .enumerate()
+            .map(|(index, step)| {
+                let y1 = step.y1;
+                let color = match step.colour {
+                    BarColor::Orange => Colour::from_rgb(255, 127, 80),
+                    BarColor::Green => Colour::from_rgb(50, 205, 50),
+                    BarColor::Grey => Colour::from_rgb(192, 192, 192),
+                };
+                Bar::new(move |_: &MyData| y1).with_colour(color).with_placement(BarPlacement::Zero).with_gap(0.1).with_group_gap(0.05)
             })
-            .collect::<Vec<Color>>()
-    });
+            .collect::<Vec<_>>()
+    })});
 
-    let options = BarChartOptions { max_ticks: 4 };
+    let series = Signal::derive(move || {Series::new(|step: &MyData| step.x).bars(bars_data().iter().cloned())});
 
-    // Define the step values for the current step
-    let step = create_memo(move |_| {
-        let current_index = current_step();
-        steps()[current_index].clone()
-    });
+
 
     view! {
-        <BarChart
-            values=Signal::derive(move || step()).into()
-            pallete=Signal::derive(move || Palette(palette())).into()
-            options=options
-            attr:style="margin-top:5px"
-            attr:preserveAspectRatio="none"
-            attr:width="500"
-            attr:height="500"
-        />
-        <button on:click=move |_| {
-            set_current_step.update(|n| *n -= 1);
-        }>"Previous step"</button>
-        <button
-            prop:disabled=move || { if disable() { true } else { false } }
-            on:click=move |_| {
-                if current_step() < steps().len() - 1 {
-                    set_current_step.update(|n| *n += 1);
+        <div>
+            {move || {
+                if !data.with(|d| d.steps.data.is_empty()) {
+                    view! {
+                        <div class="flex justify-center mt-2">
+                        <Chart
+                            aspect_ratio=AspectRatio::from_outer_height(500.0, 1.0)
+                            debug=debug
+                            series=series()
+                            data=current_data
+                            left=TickLabels::aligned_floats()
+                            inner=[
+                                AxisMarker::left_edge().into_inner(),
+                                AxisMarker::bottom_edge().into_inner(),
+                                YGridLine::default().into_inner(),
+                            ]
+                        />
+                        </div>
+                        <p>"Current step: " {current_step}</p>
+                        <p>"Total steps: " {move || data.with(|d| d.steps.data.len())}</p>
+                    }
                 } else {
-                    set_disable(true)
+                    view! { <p>"No data available. Please submit some data."</p>
+                <p></p> }
                 }
-            }
-        >
-            "Next step"
-        </button>
-        // <p>"step: "{move || step().to_string()}</p>
-        <p>"current step: "{move || current_step().to_string()}</p>
-        <p>"steps.len: "{move || steps().len().to_string()}</p>
+            }}
+            <div class="items-center">
+                <button
+                    on:click=move |_| {
+                        current_step.update(|n| {
+                            if *n > 0 { *n -= 1 }
+                        });
+                    }
+                    disabled=move || current_step.get() == 0
+                >
+                    "Previous step"
+                </button>
+                <button
+                    on:click=move |_| {
+                        current_step.update(|n| {
+                            if *n < data.with(|d| d.steps.data.len().saturating_sub(1)) {
+                                *n += 1
+                            }
+                        });
+                    }
+                    disabled=move || {
+                        let max_step = data.with(|d| d.steps.data.len().saturating_sub(1));
+                        current_step.get() >= max_step
+                    }
+                >
+                    "Next step"
+                </button>
+            </div>
+        </div>
     }
 }
--- leptos/algorithm-visualisation/src/sorting_menu.rs
+++ leptos/algorithm-visualisation/src/sorting_menu.rs
@@ -1,17 +1,19 @@
 use std::{str::FromStr, vec};
 
 use leptos::*;
-use leptos_router::{Route, RouteProps, Router, RouterProps, Routes, RoutesProps};
-use crate::sorting::{bubble_sort, BarColor, SortType};
-use crate::sorting_chart::SortingChart;
+use crate::sorting::{bubble_sort, bubble_sort_test, BarColor, SortRes, SortType, StepsTest};
+use crate::sorting_chart::{//SortingChart, 
+    SortingChartistry};
 
 
 #[component]
 pub fn SortingMenu() -> impl IntoView {
     let (sorting_type, set_sorting_type) = create_signal(SortType::Bubble);
     let (sorted_vec, set_sorted_vec) = create_signal(vec![vec![1.0]]);
-    let (palletes, set_palletes) = create_signal(vec![vec![BarColor::Green]]);
+    // let (palletes, set_palletes) = create_signal(vec![vec![BarColor::Green]]);
     let (input_value, set_input_value) = create_signal("".to_string());
+    let (data, set_data) = create_signal(SortRes::new(StepsTest::new()));
+    let debug = create_signal(true);
     let input_element: NodeRef<html::Input> = create_node_ref();
     let on_submit = move |ev: leptos::ev::SubmitEvent| {
         ev.prevent_default();
@@ -23,39 +25,57 @@ pub fn SortingMenu() -> impl IntoView {
         set_input_value(value);
         let read_vector = create_vec_from_string(input_value.get());
         let result = match sorting_type() {
-            SortType::Bubble => bubble_sort(read_vector),
-            SortType::Quick => bubble_sort(read_vector),
-            SortType::Insert => bubble_sort(read_vector),
-            SortType::Merge => bubble_sort(read_vector),
+            SortType::Bubble => bubble_sort_test(read_vector),
+            SortType::Quick => bubble_sort_test(read_vector),
+            SortType::Insert => bubble_sort_test(read_vector),
+            SortType::Merge => bubble_sort_test(read_vector),
         };
-        set_palletes(result.steps.palette);
-        set_sorted_vec(result.steps.steps);
+        // set_palletes(result.steps.palette);
+        // set_sorted_vec(result.steps.steps);
+        set_data(result);
     };
 
     view! {
-        <div>
-            <select
-                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                on:change=move |ev| {
-                    set_sorting_type(SortType::from_str(&event_target_value(&ev)).unwrap());
-                }
-                prop:value=move || sorting_type.get().to_string()
-            >
-                <option value=SortType::Bubble>"Bubble Sort"</option>
-                <option value=SortType::Insert>"Insert Sort"</option>
-                <option value=SortType::Quick>"Quick Sort"</option>
-                <option value=SortType::Merge>"Merge Sort"</option>
-            </select>
-            <form on:submit=on_submit>
-                <input type="text" value=input_value node_ref=input_element />
-                <input type="submit" value="Submit" />
-            </form>
-            <SortingChart steps=sorted_vec palettes=palletes />
+        <div class="w-screen h-screen">
+            <div>
+                <SortingChartistry debug=debug.0.into() data=data.into() />
+                // <SortingChart steps=sorted_vec palettes=palletes />
+                <div class="items-center flex flex-auto justify-center basis-full scale-125">
+                    <select
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        on:change=move |ev| {
+                            set_sorting_type(SortType::from_str(&event_target_value(&ev)).unwrap());
+                        }
+                        prop:value=move || sorting_type.get().to_string()
+                    >
+                        <option value=SortType::Bubble>"Bubble Sort"</option>
+                        <option value=SortType::Insert>"Insert Sort"</option>
+                        <option value=SortType::Quick>"Quick Sort"</option>
+                        <option value=SortType::Merge>"Merge Sort"</option>
+                    </select>
+                    <form on:submit=on_submit class="flex">
+                        <input
+                            type="text"
+                            value=input_value
+                            node_ref=input_element
+                            class="ml-2 block text-sm font-medium text-gray-900 dark:text-white bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500 p-2.5"
+                        />
+                        <input
+                            type="submit"
+                            value="Submit"
+                            class="ml-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        />
+                    </form>
+                </div>
+            </div>
 
-            <p>
-                "input value:"{move || input_value.get()} "\n" "vec_to_sort: "
-                {move || sorted_vec.get()}
-            </p>
+            <div class="fixed bottom-0 items-center">
+
+                <p>
+                    "input value: " {move || input_value.get()} "\n" "data: "
+                    {move || format!("{:?}", data.get())}
+                </p>
+            </div>
         </div>
     }
 }
